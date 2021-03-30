3d423722c62557b4693d11f518514d0b868d9276
LeoneBacciu <leonebacciu@gmail.com>
2021-03-30 21:04:08 +0200

    Add Floor includes

--- src/Robot.hpp
+++ src/Robot.hpp
@@ -2,16 +2,19 @@
 
 #if _EXECUTION_ENVIRONMENT == 0
 #include "MainMaze/robot/lib/Compass/Compass.hpp"
+#include "MainMaze/robot/lib/Floor/Floor.hpp"
 #include "MainMaze/robot/lib/Serial/SerialPort.hpp"
 #include "MainMaze/robot/lib/Temp/Temp.hpp"
 #else
 
 #include <SerialPort.hpp>
 #include <Compass.hpp>
+#include <Lasers.hpp>
+#include <Gyro.hpp>
 #include <Temp.hpp>
+#include <Floor.hpp>
 
 #endif
-#include "MainMaze/robot/lib/Floor/Floor.hpp"
 
 
 class Robot
